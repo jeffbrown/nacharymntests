4e55967c1edf575d62e5a25ded34b6df6c183e66
Jeff Scott Brown <brownj@objectcomputing.com>
2019-06-10 10:32:36 -0500

    Fix test that wasn't running

--- src/test/java/nacharymntests/DeclarativeClientWithTestFrameworkTest.java
+++ src/test/java/nacharymntests/DeclarativeClientWithTestFrameworkTest.java
@@ -3,10 +3,28 @@ package nacharymntests;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+class DeclarativeClientWithTestFrameworkTest {
+
+    @Inject
+    EmployeeClient client;
+
+    @Test
+    void testHello() {
+        assertEquals("Hello Jeff", client.hello("Jeff"));
+    }
+
+    @Test
+    void testGoodbye() {
+        assertEquals("Goodbye Jeff", client.goodbye("Jeff"));
+    }
+}
 
 @Client(value = "/", path = "/employee")
 interface EmployeeClient {
@@ -16,19 +34,4 @@ interface EmployeeClient {
 
     @Get("/goodbye/{name}")
     String goodbye(String name);
-}
-
-@MicronautTest
-class DeclarativeClientWithTestFrameworkTest {
-
-    @Inject
-    EmployeeClient client;
-
-    void testHello() {
-        assertEquals("Hello Jeff", client.hello("Jeff"));
-    }
-
-    void testGoodbye() {
-        assertEquals("Goodbye Jeff", client.goodbye("Jeff"));
-    }
 }
